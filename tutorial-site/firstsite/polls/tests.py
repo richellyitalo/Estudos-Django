f6f6f9466f42593e2689b35b822fbdad9e518ebc
Richelly Italo <richellyitalo@gmail.com>
2018-04-25 18:07:21 -0300

    Iniciando testes

--- tutorial-site/firstsite/polls/tests.py
+++ tutorial-site/firstsite/polls/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionModelTest(TestCase):
+    def testar_se_a_data_publicada_no_futuro_retorna_false(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
