12588ae0a51b7f3fb2d9d14251597943067264b2
Richelly Italo <richellyitalo@gmail.com>
2018-09-27 17:30:21 -0300

    Removido tutorial do site

--- tutorial-site/firstsite/enquete/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Pergunta
-
-admin.site.register(Pergunta)
--- tutorial-site/firstsite/enquete/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EnqueteConfig(AppConfig):
-    name = 'enquete'
