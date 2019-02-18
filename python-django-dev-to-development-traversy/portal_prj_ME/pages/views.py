bdd9f0a54fe0894e0f3d5ea6e8627c4b72a11e9e
Richelly Italo <richellyitalo@gmail.com>
2019-02-18 00:14:53 -0300

    django~traversy: single do Listing; Listings no _ME

--- python-django-dev-to-development-traversy/portal_prj_ME/pages/views.py
+++ python-django-dev-to-development-traversy/portal_prj_ME/pages/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from realtors.models import Realtor
+
 def index(request):
     return render(request, 'pages/home.html')
 
 def sobre(request):
-    return render(request, 'pages/sobre.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtors = Realtor.objects.filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'pages/sobre.html', context)
