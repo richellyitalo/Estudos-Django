47eca88d15a055d9ce7a73e359ddf6251f414893
Richelly Italo <richellyitalo@gmail.com>
2019-02-19 01:31:58 -0300

    django~traversy: listings _ME

--- python-django-dev-to-development-traversy/portal_prj_ME/listings/views.py
+++ python-django-dev-to-development-traversy/portal_prj_ME/listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator
 
 from .models import Listing
@@ -15,9 +15,12 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+
     context = {
-        'listing_id': listing_id
+        'listing': listing
     }
+
     return render(request, 'listings/listing.html', context)
 
 def search(request):
