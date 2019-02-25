520d87e941942d81947f934b4431b79855a9e909
Richelly Italo <richellyitalo@gmail.com>
2019-02-25 01:34:01 -0300

    django~traversy: registro/login via site (+ME)

--- python-django-dev-to-development-traversy/portal_prj/accounts/views.py
+++ python-django-dev-to-development-traversy/portal_prj/accounts/views.py
@@ -1,17 +1,53 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Erro ao cadastrar')
-        return redirect('register')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username já está cadastrado')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'E-mail já está cadastrado')
+                return redirect('register')
+            else:
+                # OK
+                user = User.objects.create_user(username, email, password, first_name=first_name, last_name=last_name)
+                user.save()
+                # Logando após registrar
+                # auth.login(request, user)
+                # messages.success(request, 'Usuário criado e logado com sucesso')
+                # return redirect('index')
+                messages.success(request, 'Conta criada. Agora você precisa se autenticar')
+                return redirect('login')
+        else:
+            messages.error(request, 'As senhas não estão iguais')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
 def login(request):
     if request.method == 'POST':
-        # autenticar
-        return redirect('login')
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'Agora você está logado')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Dados inválidos')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
--- python-django-dev-to-development-traversy/portal_prj_ME/accounts/views.py
+++ python-django-dev-to-development-traversy/portal_prj_ME/accounts/views.py
@@ -1,21 +1,56 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
 
 def register(request):
     if request.method == 'POST':
-        # Cadastra novo usuário
-        messages.error(request, 'Dados inválidos')
-        return render(request, 'accounts/register.html')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password != password2:
+            messages.error(request, 'Senha de confirmação deve ser igual a senha')
+            return redirect('register')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username em uso')
+            return redirect('register')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'E-mail em uso')
+            return redirect('register')
+            
+        user = User.objects.create_user(username, email, password, first_name=first_name, last_name=last_name)
+        user.save()
+
+        # Autenticar
+        # auth.login(request, user)
+        # messages.success(request, 'Conta criada e autenticada')
+        # return redirect('home')
+
+        messages.success(request, 'Conta criada  com sucesso. Realize login')
+        return redirect('login')
     else:
         return render(request, 'accounts/register.html')
 
 def login(request):
     if request.method == 'POST':
         # Autenticação
-        print('Autenticação')
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            messages.success(request, 'Autenticado com sucesso')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Dados inválidos')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
     
